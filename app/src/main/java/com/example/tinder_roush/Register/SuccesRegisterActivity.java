b30b7b16d3272f6e06083c4899f72a989dee4d32
aleja <alejandra@loducode.com>
2022-06-01 15:57:54 -0500

    Maquetación vista “Cuenta creada exitosamente”

--- app/src/main/java/com/example/tinder_roush/Register/SuccesRegisterActivity.java
+++ app/src/main/java/com/example/tinder_roush/Register/SuccesRegisterActivity.java
@@ -2,15 +2,45 @@ package com.example.tinder_roush.Register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.tinder_roush.Home.HomeActivity;
 import com.example.tinder_roush.R;
+import com.example.tinder_roush.RecoveryPassword.RecoveryPasswordActivity;
 
 public class SuccesRegisterActivity extends AppCompatActivity {
+
+    Button okey_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succes_register);
+        initObjets();
+        listeners();
     }
+
+    private void initObjets(){
+        okey_button = findViewById(R.id.ok_button);
+    }
+
+    private void listeners() {
+
+        okey_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performHome();
+            }
+        });
+
+    }
+
+    public void performHome(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
 }
