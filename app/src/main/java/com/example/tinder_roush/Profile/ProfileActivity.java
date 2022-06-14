d715749d417b8b4489e331b6ae82f0e1c4d84c4d
aleja <alejandra@loducode.com>
2022-06-14 09:02:53 -0500

    Botón volver al login + detalles

--- app/src/main/java/com/example/tinder_roush/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/tinder_roush/Profile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.tinder_roush.MenuNavigation.MenuNavigation;
 import com.example.tinder_roush.R;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Button backHome;
+    ImageButton backHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
